9eda509da3c5351b9ac43b33de87779d9f4bab18
Dallin <ijiff08@gmail.com>
2025-03-22 11:00:46 -0600

    add ParentNode and tests

--- src/htmlnode.py
+++ src/htmlnode.py
@@ -34,3 +34,24 @@ class LeafNode(HTMLNode):
             return self.value
         return f"<{self.tag}{self.props_to_html()}>{self.value}</{self.tag}>"
         
+
+class ParentNode(HTMLNode):
+    def __init__(self, tag, children, props=None):
+        super().__init__(tag, None, children, props)
+
+    def to_html(self):
+        if self.tag is None:
+            raise ValueError("ParentNode must have a tag")
+        if self.children is None:
+            raise ValueError("ParentNode must have children")
+        html = f"<{self.tag}>"
+        for child in self.children:
+            html += child.to_html()
+        html += f"</{self.tag}>"
+        return html
+
+
+
+
+
+
--- src/test_htmlnode.py
+++ src/test_htmlnode.py
@@ -25,3 +25,28 @@ class TestLeafNode(unittest.TestCase):
         node = LeafNode("a", "This is a link", {"href": "www.Rickrolled.gov"})
         self.assertEqual(node.to_html(), "<a href=www.Rickrolled.gov>This is a link</a>")
         
+
+class TestParentNode(unittest.TestCase):
+    def test_to_html_with_children(self):
+        child_node = LeafNode("span", "child")
+        parent_node = ParentNode("div", [child_node])
+        self.assertEqual(parent_node.to_html(), "<div><span>child</span></div>")
+
+    def test_to_html_with_grandchildren(self):
+        grandchild_node = LeafNode("b", "grandchild")
+        child_node = ParentNode("span", [grandchild_node])
+        parent_node = ParentNode("div", [child_node])
+        self.assertEqual(
+            parent_node.to_html(),
+            "<div><span><b>grandchild</b></span></div>",
+        )
+
+    def test_multiple_parents(self):
+        child = LeafNode(None, "child")
+        child1 = LeafNode("i", "child1")
+        parent = ParentNode("div", [child, child1])
+        child2 = LeafNode(None, "child2")
+        child3 = LeafNode("b", "child3")
+        parent1 = ParentNode("p", [child2, child3])
+        final_parent = ParentNode("header", [parent, parent1])
+        self.assertEqual(final_parent.to_html(), "<header><div>child<i>child1</i></div><p>child2<b>child3</b></p></header>")
